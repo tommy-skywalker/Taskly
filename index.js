50718a3024f905da37f2f50f67d43537b6840f0d
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2022-03-05 09:15:00 +0100

    Add static file serving for frontend assets

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let tasks = [];
 
